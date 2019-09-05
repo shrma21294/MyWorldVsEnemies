5da70592e87a601436be51698797cadb46003bdb
Komal Sharma <komalsharma@Komals-MBP.home>
2019-09-05 03:53:32 -0400

    Added self destruction and score board functionality

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -5,15 +5,26 @@ using UnityEngine;
 public class Enemy : MonoBehaviour
 {
 	[SerializeField] GameObject deathFX;
+	[SerializeField] Transform parent;
+
+	ScoreBoard scoreBoard;
+
     // Start is called before the first frame update
     void Start()
     {
-        Collider boxCollider = gameObject.AddComponent<BoxCollider>();
+        AddBoxCollider();
+        scoreBoard = FindObjectOfType<ScoreBoard>();
+    }
+
+    private void AddBoxCollider(){
+    	Collider boxCollider = gameObject.AddComponent<BoxCollider>();
         boxCollider.isTrigger= false;
     }
 
     void OnParticleCollision(GameObject other){
-    	Instantiate(deathFX, transform.position, Quaternion.identity);
+    	scoreBoard.ScoreHit();
+    	GameObject fx = Instantiate(deathFX, transform.position, Quaternion.identity);
+    	fx.transform.parent = parent;
     	Destroy(gameObject);
     }
 }
